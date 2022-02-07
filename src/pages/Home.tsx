19bed62ecd7eadfc810582bac9c0ba9ca4b5de81
Felipe <felipe@pop-os.localdomain>
2022-02-07 17:05:41 -0300

    authenticate + firebase

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,28 @@
+import { useContext } from 'react';
+import { useHistory } from 'react-router-dom'
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg';
 import googleIconImg from '../assets/images/google-icon.svg'
 import { Button } from '../components/Button';
+import { auth, firebase } from '../services/firebase'
 
+import { TestContext } from '../App';
 
 import '../styles/Auth.scss';
 
-export const Home = () =>{
+export const Home = () => {
+  const history = useHistory();
+  const value = useContext(TestContext);
+
+  function handleCreateRoom() {
+    const provider = new firebase.auth.GoogleAuthProvider()
+
+    auth.signInWithPopup(provider).then(result => {
+      console.log(result)
+    })
+    history.push('/rooms/new')
+  }
+
   return (
     <div id='page-auth'>
       <aside>
@@ -16,19 +32,19 @@ export const Home = () =>{
       </aside>
       <main>
         <div className='main-content'>
-        <img src={logoImg} alt="Logo Letmeask" />
-        <button className='create-room'>
-          <img src={googleIconImg} alt="Logo do Google" />
-          Crie sua sala com o Google
-        </button>
-        <div className='separator'>ou entre em uma sala</div>
-        <form >
-          <input 
-          type="text" 
-          placeholder='Digite o código da sala'
-          />
-          <Button type='submit'>Entrar na sala</Button>
-        </form>
+          <img src={logoImg} alt="Logo Letmeask" />
+          <button onClick={handleCreateRoom} className='create-room' >
+            <img src={googleIconImg} alt="Logo do Google" />
+            Crie sua sala com o Google
+          </button>
+          <div className='separator'>ou entre em uma sala</div>
+          <form >
+            <input
+              type="text"
+              placeholder='Digite o código da sala'
+            />
+            <Button type='submit'>Entrar na sala</Button>
+          </form>
         </div>
       </main>
     </div>
